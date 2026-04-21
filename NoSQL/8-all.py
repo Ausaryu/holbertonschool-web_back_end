1e418686c118cf9d22e774df317ec1ea166dd567
Ausaryu <yonas.houriez@gmail.com>
2026-04-21 14:17:04 +0200

    add documenttion to my function

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ 8-all """
-from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
+    """ list all """
     result = []
 
     for doc in mongo_collection.find():
