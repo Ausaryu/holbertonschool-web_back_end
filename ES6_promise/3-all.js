561f9449f44c27eb9653b7b6818cd45b0585acc2
Ausaryu <yonas.houriez@gmail.com>
2026-05-06 14:01:52 +0200

    feat: add initial project setup with package.json and utility functions
    - Created package.json with scripts for linting, testing, and development.
    - Added devDependencies for Babel, ESLint, and Jest.
    - Implemented utility functions: uploadPhoto and createUser that return resolved promises.

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+export default function handleProfileSignup () {
+  const photo = uploadPhoto();
+  const user = createUser();
+
+  Promise.all([photo, user])
+    .then((results) => {
+      console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
