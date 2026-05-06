b1f3d24dc11e2b8f47be1c65f2005d53a4c2120e
Ausaryu <yonas.houriez@gmail.com>
2026-05-06 14:09:26 +0200

    fix: ensure consistent error handling in guardrail function and improve promise handling in profile signup

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup () {
   const photo = uploadPhoto();
   const user = createUser();
 
-  Promise.all([photo, user])
+  return Promise.all([photo, user])
     .then((results) => {
       console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
     })
